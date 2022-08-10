22c076d8b50495ffc568a90c3ea9073df94e3d54
Jorge de Paz <jorge.depazcabanas@gmail.com>
2022-08-10 10:36:05 +0200

    education experinece component added

---
+++ src/app/components/edexp/edexp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edexp',
+  templateUrl: './edexp.component.html',
+  styleUrls: ['./edexp.component.css']
+})
+export class EdexpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
