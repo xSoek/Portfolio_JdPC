d3d2303ccf85d310ac5ce32f30af148c092dec11
Jorge de Paz <jorge.depazcabanas@gmail.com>
2022-08-12 11:32:27 +0200

    about me structure finished

--- src/app/components/who-i-am/who-i-am.component.ts
+++ src/app/components/who-i-am/who-i-am.component.ts
@@ -10,6 +10,14 @@ export class WhoIAmComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let articles = document.querySelectorAll("article");
+
+    articles.forEach(article => {
+      article.addEventListener("mouseenter", () => {
+        article.style.width = "100%"
+        article.style.backgroundColor = "rgba(255, 255, 255, 0.1)"
+      })
+    })
   }
 
 }
