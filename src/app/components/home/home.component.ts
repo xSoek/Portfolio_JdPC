34bf2b0c2514048c77b2b266a5bf94238b5d6a2f
Jorge de Paz <jorge.depazcabanas@gmail.com>
2022-08-12 11:32:02 +0200

    home structure finished

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as AOS from "aos";
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,7 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    AOS.init();
   }
 
 }
